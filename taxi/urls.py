fb9d0b2772d3d7e03133a269849484705dc813ec
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-14 18:15:27 +0200

    task

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,7 +11,7 @@ from .views import (
     CarDeleteView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    ManufacturerDeleteView, CarCreateView,
 )
 
 urlpatterns = [
@@ -32,7 +32,7 @@ urlpatterns = [
          name="manufacturer-delete"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create", CarListView.as_view(), name="car-create"),
+    path("cars/create", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
